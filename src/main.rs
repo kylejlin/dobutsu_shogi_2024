3cc5e5da86c4a535d5a96b2fa87fa41f8becb18b
Kyle Lin <kylejlin.us@gmail.com>
2024-11-02 06:52:18 -0700

    Print elapsed time

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use std::fs;
 use std::path::Path;
+use std::time::Instant;
 
 fn main() {
+    println!("Starting tree search. This will probably take several hours.");
+    let now = Instant::now();
+    let solution_map = dobutsu_shogi_2024::calculate();
+    let elapsed = now.elapsed();
+    println!("Completed tree search. It took {:?}.", elapsed);
+
     let path = Path::new(file!())
         .parent()
         .unwrap()
         .parent()
         .unwrap()
         .join("solution.dat");
-    let solution_map = dobutsu_shogi_2024::calculate();
     let bytes = solution_map.to_bytes();
     fs::write(&path, bytes).unwrap();
 }
