4104296161d1cc534a17637497a7a789288b725b
Kyle Lin <kylejlin.us@gmail.com>
2024-11-02 04:10:25 -0700

    Add SolutionCache

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,19 @@
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Solution(pub u64);
+
 const PLY_LIMIT: u8 = 200;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct SearchNode(pub u64);
+struct SearchNode(pub u64);
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct SearchQuasinode(pub u64);
+struct SearchQuasinode(pub u64);
 
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub struct Solution(pub u64);
+struct SolutionCache {
+    raw: [CacheNode<CacheNode<CacheNode<CacheNode<CacheNode<[u64; 16]>>>>>; 256 * 256],
+}
+
+type CacheNode<T> = [Option<Box<T>>; 16];
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct SolutionMap {
@@ -19,11 +25,7 @@ impl SearchNode {
         todo!()
     }
 
-    fn record_terminal_child_outcome(&mut self, terminal_node: SearchQuasinode) {
-        todo!()
-    }
-
-    fn record_nonterminal_child_outcome(&mut self, solved_node: SearchNode) {
+    fn record_solution(&mut self, solution: Solution) {
         todo!()
     }
 
@@ -42,17 +44,53 @@ impl SearchQuasinode {
     }
 }
 
+impl From<SearchNode> for Solution {
+    fn from(node: SearchNode) -> Self {
+        Solution(node.0)
+    }
+}
+
+impl From<SearchQuasinode> for Solution {
+    fn from(quasinode: SearchQuasinode) -> Self {
+        Solution(quasinode.0)
+    }
+}
+
 impl From<SearchQuasinode> for SearchNode {
     fn from(quasinode: SearchQuasinode) -> Self {
         SearchNode(quasinode.0)
     }
 }
 
+impl SolutionCache {
+    fn new() -> SolutionCache {
+        let empty: CacheNode<CacheNode<CacheNode<CacheNode<CacheNode<[u64; 16]>>>>> =
+            Default::default();
+        let mut v = Vec::with_capacity(256 * 256);
+        for _ in 0..256 * 256 {
+            v.push(empty.clone());
+        }
+        SolutionCache {
+            raw: v.try_into().unwrap(),
+        }
+    }
+
+    fn get(&self, node: SearchNode) -> Option<Solution> {
+        todo!()
+    }
+}
+
+impl From<SolutionCache> for SolutionMap {
+    fn from(cache: SolutionCache) -> Self {
+        todo!()
+    }
+}
+
 pub fn calculate() -> SolutionMap {
+    let mut solution_cache = SolutionCache::new();
+
     let mut stack: Vec<SearchNode> = Vec::with_capacity(PLY_LIMIT as usize);
     stack.push(SearchNode::initial());
-
-    let mut solution_map = SolutionMap { raw: vec![] };
 
     loop {
         let last_node = stack.last().unwrap().clone();
@@ -65,10 +103,7 @@ pub fn calculate() -> SolutionMap {
                 break;
             }
 
-            stack
-                .last_mut()
-                .unwrap()
-                .record_nonterminal_child_outcome(last_node);
+            stack.last_mut().unwrap().record_solution(last_node.into());
 
             continue;
         }
@@ -77,14 +112,21 @@ pub fn calculate() -> SolutionMap {
         let new_quasinode = last_node.explore(explorer_index);
 
         if new_quasinode.clone().is_terminal() {
-            last_node.record_terminal_child_outcome(new_quasinode);
+            last_node.record_solution(new_quasinode.into());
             continue;
         }
 
-        stack.push(new_quasinode.into())
+        let new_node: SearchNode = new_quasinode.into();
+
+        if let Some(solution) = solution_cache.get(new_node.clone()) {
+            last_node.record_solution(solution);
+            continue;
+        }
+
+        stack.push(new_node);
     }
 
-    solution_map
+    solution_cache.into()
 }
 
 pub const EXPLORERS: [fn(&mut SearchNode) -> SearchQuasinode; 128] = [todo_dummy; 128];
