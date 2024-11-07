7604d91cd18f70934ea34466a5d44680e929129f
Kyle Lin <kylejlin.us@gmail.com>
2024-11-06 20:00:36 -0800

    Add ForwardNode::initial

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ use std::fs;
 use std::path::Path;
 use std::time::Instant;
 
+use dobutsu_shogi_2024::forward::*;
+
 fn main() {
     println!("Starting tree search. This will probably take several hours.");
     let now = Instant::now();
-    let solution_map = dobutsu_shogi_2024::calculate();
+    let reachable_states = reachable_states(ForwardNode::initial());
     let elapsed = now.elapsed();
     println!("Completed tree search. It took {:?}.", elapsed);
 
@@ -15,6 +17,14 @@ fn main() {
         .parent()
         .unwrap()
         .join("solution.dat");
-    let bytes = solution_map.to_bytes();
+    let bytes = reachable_states_to_bytes(&reachable_states);
     fs::write(path, bytes).unwrap();
+}
+
+fn reachable_states_to_bytes(reachable_states: &[BackwardNode]) -> Vec<u8> {
+    let mut bytes = Vec::with_capacity(reachable_states.len() * std::mem::size_of::<u64>());
+    for state in reachable_states {
+        bytes.extend_from_slice(&state.0.to_le_bytes())
+    }
+    bytes
 }
