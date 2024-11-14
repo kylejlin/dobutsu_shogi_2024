5a3011f5d9b784016056245d2c4f5b28937d6d7f
Kyle Lin <kylejlin.us@gmail.com>
2024-11-14 03:43:06 -0800

    Reformat every_child_lists_parent_as_parent error message

--- src/tests/parent_child_consistency.rs
+++ src/tests/parent_child_consistency.rs
@@ -3,18 +3,18 @@ use super::*;
 #[ignore]
 #[test]
 fn every_child_lists_parent_as_parent() {
-    fuzz(10_000, |state| {
-        state.visit_children(|child| {
+    fuzz(10_000, |parent| {
+        parent.visit_children(|child| {
             let mut found_parent = false;
-            child.visit_parents(|parent| {
-                found_parent |= parent == state;
+            child.visit_parents(|child_parent| {
+                found_parent |= child_parent == parent;
             });
             if !found_parent {
-                let parent = state.pretty();
+                let parent = parent.pretty();
                 let parent_children = parent.0.children().pretty();
                 let child = child.pretty();
                 let child_parents = child.0.parents().pretty();
-                panic!("Child did not list parent as parent.\n\nPARENT:\n\n{parent}\n\nPARENT.CHILDREN:\n\n{parent_children}\n\nCHILD:\n\n{child}\n\nCHILD.PARENTS:\n\n{child_parents}");
+                panic!("Child did not list parent as parent.\n\nPARENT:\n\n{parent}\n\nCHILD:\n\n{child}\n\nPARENT.CHILDREN:\n\n{parent_children}\n\nCHILD.PARENTS:\n\n{child_parents}");
             }
         })
     });
