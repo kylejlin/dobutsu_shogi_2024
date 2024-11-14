4a78c68d7154048b5933c377a99bce2d786aa318
Kyle Lin <kylejlin.us@gmail.com>
2024-11-14 02:26:16 -0800

    Add allegiance_inversion_is_involutive test

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,5 +1,8 @@
 use super::{pretty::IntoPretty, *};
 
+use rand::{Rng, SeedableRng};
+use rand_xorshift::XorShiftRng;
+
 mod i9;
 mod legal_moves;
 mod parent_child_consistency;
@@ -42,3 +45,49 @@ fn initial_search_node_child0_children_are_correct() {
     let children = child0.0.children().pretty();
     insta::assert_snapshot!(format!("parent:\n{child0}\n\nchildren:\n{children}"));
 }
+
+#[test]
+fn allegiance_inversion_is_involutive() {
+    fuzz(1_000_000, |state| {
+        let state = state.into_builder();
+        let double_inverted = state.invert_active_player().invert_active_player();
+        if state != double_inverted {
+            let state = state.pretty();
+            let double_inverted = double_inverted.pretty();
+            panic!("Allegiance inversion is not involutive.\n\nSTATE:\n\n{state}\n\nDOUBLE INVERTED:\n\n{double_inverted}");
+        }
+    });
+}
+
+/// Pseudorandomly plays `game_count` games and calls `callback`
+/// for each state in each game.
+///
+/// The games are guaranteed to be the same for all invocations
+/// of `fuzz`, so you can safely use `fuzz` in your
+/// tests without introducing nondeterminism.
+pub fn fuzz<F: FnMut(SearchNode)>(game_count: usize, mut callback: F) {
+    let mut rng = XorShiftRng::from_seed(PRNG_SEED);
+    let mut child_buffer = Vec::with_capacity(8 * 12);
+
+    for _ in 0..game_count {
+        let mut state = SearchNode::initial();
+
+        callback(state);
+
+        while !state.is_terminal() {
+            child_buffer.clear();
+            state.visit_children(|child| child_buffer.push(child));
+
+            let selected_child = child_buffer[rng.gen_range(0..child_buffer.len())];
+
+            callback(selected_child);
+
+            state = selected_child;
+        }
+    }
+}
+
+/// Randomly chosen seed
+pub const PRNG_SEED: [u8; 16] = [
+    113, 8, 5, 99, 97, 161, 194, 214, 140, 140, 80, 143, 213, 130, 254, 107,
+];
--- src/tests/parent_child_consistency.rs
+++ src/tests/parent_child_consistency.rs
@@ -1,6 +1,3 @@
-use rand::{Rng, SeedableRng};
-use rand_xorshift::XorShiftRng;
-
 use super::*;
 
 #[ignore]
@@ -22,36 +19,3 @@ fn every_child_lists_parent_as_parent() {
         })
     });
 }
-
-/// Pseudorandomly plays `game_count` games and calls `callback`
-/// for each state in each game.
-///
-/// The games are guaranteed to be the same for all invocations
-/// of `fuzz`, so you can safely use `fuzz` in your
-/// tests without introducing nondeterminism.
-pub fn fuzz<F: FnMut(SearchNode)>(game_count: usize, mut callback: F) {
-    let mut rng = XorShiftRng::from_seed(PRNG_SEED);
-    let mut child_buffer = Vec::with_capacity(8 * 12);
-
-    for _ in 0..game_count {
-        let mut state = SearchNode::initial();
-
-        callback(state);
-
-        while !state.is_terminal() {
-            child_buffer.clear();
-            state.visit_children(|child| child_buffer.push(child));
-
-            let selected_child = child_buffer[rng.gen_range(0..child_buffer.len())];
-
-            callback(selected_child);
-
-            state = selected_child;
-        }
-    }
-}
-
-/// Randomly chosen seed
-pub const PRNG_SEED: [u8; 16] = [
-    113, 8, 5, 99, 97, 161, 194, 214, 140, 140, 80, 143, 213, 130, 254, 107,
-];
